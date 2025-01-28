233e30e5b6c6bdb7326532c361bfdf60bdb70221
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-01-28 03:53:43 -0300

    test: add a single teste

--- src/test/java/com/marcusvbrangel/happyschool/HappySchoolApplicationTests.java
+++ src/test/java/com/marcusvbrangel/happyschool/HappySchoolApplicationTests.java
@@ -1,13 +1,18 @@
 package com.marcusvbrangel.happyschool;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class HappySchoolApplicationTests {
 
 	@Test
+    @DisplayName("verifica se true é igual a true")
 	void contextLoads() {
+        assertTrue(false);
 	}
 
 }
