68555ff9b1692f175b8b9aa8ddf703d6e2d76980
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-02-06 21:04:10 -0300

    feat: add spring boot actuator

--- src/main/java/com/marcusvbrangel/happyschool/controller/ProfileController.java
+++ src/main/java/com/marcusvbrangel/happyschool/controller/ProfileController.java
@@ -1,8 +1,17 @@
 package com.marcusvbrangel.happyschool.controller;
 
+import com.marcusvbrangel.happyschool.model.Address;
+import com.marcusvbrangel.happyschool.model.Person;
 import com.marcusvbrangel.happyschool.model.Profile;
+import com.marcusvbrangel.happyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,13 +19,62 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ProfileController {
 
+    private final PersonRepository personRepository;
+
+    public ProfileController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @RequestMapping("displayProfile")
-    public ModelAndView displayMessages() {
+    public ModelAndView displayMessages(Model model, HttpSession httpSession) {
+        Person person = (Person) httpSession.getAttribute("loggedInPerson");
         var profile = new Profile();
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+
+        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
+
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile", profile);
         return modelAndView;
     }
 
+    @PostMapping("/updateProfile")
+    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile,
+                                Errors errors, HttpSession httpSession) {
+
+        if (errors.hasErrors()) {
+            return "profile.html";
+        }
+
+        Person person = (Person) httpSession.getAttribute("loggedInPerson");
+        person.setName(profile.getName());
+        person.setEmail(profile.getEmail());
+        person.setMobileNumber(profile.getMobileNumber());
+
+        if (person.getAddress() == null || !(person.getAddress().getAddressId() > 0)) {
+            person.setAddress(new Address());
+        }
+
+        person.getAddress().setAddress1(profile.getAddress1());
+        person.getAddress().setAddress2(profile.getAddress2());
+        person.getAddress().setCity(profile.getCity());
+        person.getAddress().setState(profile.getState());
+        person.getAddress().setZipCode(profile.getZipCode());
+
+        personRepository.save(person);
+
+        httpSession.setAttribute("loggedInPerson", person);
+
+        return "redirect:/displayProfile";
+
+    }
 
 }
